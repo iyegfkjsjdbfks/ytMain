2cb1d54b386fe5bd5a946563c49b735e8e6d682b
Anas Sabri <anassabri@gmail.com>
2025-06-03 00:47:24 +0530

    ref_4

--- src/features/video/components/index.ts
+++ src/features/video/components/index.ts
@@ -3,4 +3,5 @@ export { default as VideoCard } from './VideoCard';
 export { default as VideoList } from './VideoList';
 export { default as VideoGrid } from './VideoGrid';
 export { default as VideoUpload } from './VideoUpload';
+export { default as StudioVideoGrid } from './StudioVideoGrid';
 export * from './VideoPlayer';
