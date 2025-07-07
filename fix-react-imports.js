c1f9b972fd0c37b3907c21095830c5441d063f0f
Anas Sabri <anassabri@gmail.com>
2025-07-07 23:37:50 +0000

    83

--- fix-react-imports.js
+++ fix-react-imports.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
+import fs from 'fs';
+import path from 'path';
+import { glob } from 'glob';
 
 // Find all TypeScript React files
 const files = glob.sync('**/*.{tsx,ts}', {
