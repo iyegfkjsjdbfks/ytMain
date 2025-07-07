37c917df4970b7ed25681c7a7af6d73a72152cac
Anas Sabri <anassabri@gmail.com>
2025-07-07 21:50:08 +0000

    75

--- pages/RefactoredContentManagerPage.tsx
+++ pages/RefactoredContentManagerPage.tsx
@@ -1,5 +1,4 @@
-import type React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import BaseForm from '../components/BaseForm';
 import BaseModal from '../components/BaseModal';
