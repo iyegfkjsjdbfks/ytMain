de3c44074b6c316fa6c827818bb374a1c28f98a0
Anas Sabri <anassabri@gmail.com>
2025-07-07 22:07:53 +0000

    77

--- pages/VideoEditorPage.tsx
+++ pages/VideoEditorPage.tsx
@@ -1,5 +1,4 @@
-import type React from 'react';
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import { PlayIcon, PauseIcon, ScissorsIcon, SpeakerWaveIcon, ArrowUturnLeftIcon, ArrowUturnRightIcon, DocumentArrowDownIcon, DocumentIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
