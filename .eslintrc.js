59d514c99f4461bc89b087ea4fe0c0ae588470db
Anas Sabri <anassabri@gmail.com>
2025-08-24 07:09:23 +0530

    build(eslint): update eslint config for typescript and react support
    - Add TypeScript and React plugins with recommended rules
    - Configure JSX support and browser environment
    - Update rules for TypeScript and React best practices
    - Set React version detection

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,41 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
     project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+  plugins: ['@typescript-eslint', 'react'],
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
   ],
   root: true,
   env: {
+    browser: true,
     node: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js', 'dist/', 'node_modules/', 'coverage/'],
   rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^' }],
+    '@typescript-eslint/prefer-const': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
     'prefer-const': 'error',
     'no-var': 'error',
     'no-console': 'off',
     'no-debugger': 'error',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
